a2367c4ca0eb7ac1fc2c68981ba982ffb337f73c
ighiba <ighiba18@gmail.com>
2020-04-12 23:27:28 +0300

    Initial Commit

---
+++ ProgrammerCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammerCalc
+//
+//  Created by Ivan Ghiba on 12.04.2020.
+//  Copyright © 2020 ighiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
