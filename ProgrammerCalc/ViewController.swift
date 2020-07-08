f13333a958a3211336003acdb3bba55e7656b36a
ighiba <ighiba18@gmail.com>
2020-07-08 19:34:01 +0300

    Added titleLabels on numeric buttons

--- ProgrammerCalc/ViewController.swift
+++ ProgrammerCalc/ViewController.swift
@@ -11,15 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     var buttons: [UIButton] = []
-
+    weak var numButtonsView: UICollectionView?
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        /*
         let button = CalculatorButton.init(type: .custom)
-        button.frame = CGRect( x: 100, y: 100, width: 100.0, height: 100.0)
+        button.frame = CGRect( x: 90, y: 90, width: 90.0, height: 90.0)
+        // set tag for interation
+        button.tag = 1
         button.setTitle("0", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.backgroundColor = .white
@@ -27,21 +30,60 @@ class ViewController: UIViewController {
         button.titleLabel?.font = UIFont(name: "HelveticaNeue-Thin", size: 45.0)
         // set borders
         button.layer.borderWidth = 0.5
-        button.layer.borderColor = UIColor.black.cgColor
-        
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        // round corners
+        button.layer.cornerRadius = 35.0
         self.view.addSubview(button)
-        
+        */
+        createButtons()
+        //self.view.addSubview(createButtons())
 
     }
     @objc func numButtonAction(sender: UIButton!) {
         print("num button tapped")
     }
 
-    func createButtons() {
-        for number  in 1...9 {
-            //let button = UIButton.
-            //buttons.append(UIButton.)
+    func createButtons()  {
+        //===========
+        // Properties
+        // ==========
+        var buttonLabel: Int = 9
+        //var buttons: [UIButton] = []
+        
+        for row in 0...2{
+            
+            buttonLabel = buttonLabel - 2
+            
+            for column  in 0...2 {
+                
+                let button = CalculatorButton.init(type: .custom)
+                button.frame = CGRect( x: Double(100*column + 20), y: Double(100*row + 300), width: 90.0, height: 90.0)
+                    
+                // set tag for interation
+                button.tag = buttonLabel
+                button.addTarget(self, action: #selector(toucUhpOutsideAction), for: [.touchDragExit, .touchDragOutside])
+                button.setTitle(String(buttonLabel), for: .normal)
+                button.setTitleColor(.black, for: .normal)
+                button.backgroundColor = .white
+                // set font size, font family
+                button.titleLabel?.font = UIFont(name: "HelveticaNeue-Thin", size: 45.0)
+                // set borders
+                button.layer.borderWidth = 0.5
+                button.layer.borderColor = UIColor.lightGray.cgColor
+                // round corners
+                button.layer.cornerRadius = 35.0
+                
+                buttonLabel += 1
+
+                self.view.addSubview(button)
+            }
+            buttonLabel = buttonLabel - 4
         }
+        //return buttons
+    }
+    @objc func toucUhpOutsideAction(sender: UIButton) {
+        //print("touchUpOutside")
+        //sender.isHighlighted = false
     }
 
 
@@ -52,6 +94,7 @@ class ViewController: UIViewController {
 //
 
 class CalculatorButton: UIButton {
+    private let _boundsExtension: CGFloat = 0
     // override isHighlighted for calculator buttons
     override open var isHighlighted: Bool {
         // if variable state changed
@@ -73,9 +116,10 @@ class CalculatorButton: UIButton {
                     options: [.curveEaseOut, .beginFromCurrentState, .allowUserInteraction],
                     animations: { self.backgroundColor = .white },
                     completion: nil)
-            }  
+            }
         }
     }
+    
 }
 
 
