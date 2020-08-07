5aecafef497622c525bbd69f013ce28855a4116d
ighiba <ighiba18@gmail.com>
2020-08-07 11:22:33 +0300

    Added SettingsModdel for storing settings data

---
+++ ProgrammerCalc/Model/SettingsModel.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsModel.swift
+//  ProgrammerCalc
+//
+//  Created by Ivan Ghiba on 06.08.2020.
+//  Copyright © 2020 ighiba. All rights reserved.
+//
+
+import Foundation
+
+class SettingsModel {
+    var darkMode: Bool
+    var tappingSounds: Bool
+    var hapticFeedback: Bool
+    
+    init(darkMode: Bool, tappingSounds: Bool, hapticFeedback: Bool) {
+        self.darkMode = darkMode
+        self.tappingSounds = tappingSounds
+        self.hapticFeedback = hapticFeedback
+    }
+}
