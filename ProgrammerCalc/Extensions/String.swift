9a56f45a7ce0aec6d11c70b8c2e96e53545a7f71
ighiba <ighiba18@gmail.com>
2021-10-07 17:41:38 +0300

    Added Decimal and String extensions for convertint DEC -> BIN and BIN -> DEC not using Integer values

--- ProgrammerCalc/Extensions/String.swift
+++ ProgrammerCalc/Extensions/String.swift
@@ -9,6 +9,32 @@
 import Foundation
 
 extension String {
+    // convert decimal to binary str
+    init(_ valueDec: Decimal, radix: Int) {
+        self.init()
+        var dec = valueDec
+        var decCopy = dec
+        // round decimal
+        NSDecimalRound(&dec, &decCopy, 0, .down)
+        
+        var str = String()
+        
+        while dec > 0 {
+            autoreleasepool {
+                // get reminder
+                let reminder =  dec % Decimal(radix)
+                str.append("\(reminder)")
+                // divide dec
+                dec = dec / Decimal(radix)
+                decCopy = dec
+                // round dec
+                NSDecimalRound(&dec, &decCopy, 0, .down) 
+            }
+        }
+        // reverse string
+        self = String(str.reversed())
+    }
+
     // removing all spaces from string
     func removeAllSpaces() -> String {
         return self.replacingOccurrences(of: " ", with: "")
