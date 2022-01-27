3710c3c88c3ab31fc964b0689b30b0ad472d62fe
ighiba <ighiba18@gmail.com>
2022-01-27 22:10:45 +0300

    Added BitButton

---
+++ ProgrammerCalc/View/PCalcView/BitButton.swift
@@ -0,0 +1,13 @@
+//
+//  BitButton.swift
+//  ProgrammerCalc
+//
+//  Created by Ivan Ghiba on 27.01.2022.
+//  Copyright © 2022 ighiba. All rights reserved.
+//
+
+import UIKit
+
+class BitButton: UIButton {
+    
+}
