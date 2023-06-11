496a0d2cee89f6da41ca1fe9a406d740399a2358
ighiba <ighiba18@gmail.com>
2023-06-11 13:45:06 +0300

    Minor fixes

--- ProgrammerCalc/View/CalculatorView/ButtonsViewControllerPad.swift
+++ ProgrammerCalc/View/CalculatorView/ButtonsViewControllerPad.swift
@@ -73,7 +73,7 @@ class ButtonsViewControllerPad: UIViewController, ButtonsContainerControllerProt
 
                 // initialize button by type
                 switch title {
-                case "A","B","C","D","E","F","00","FF","0"..."9",".":
+                case "A","B","C","D","E","F","00","FF","0","1","2","3","4","5","6","7","8","9",".":
                     button = CalculatorButton(calcButtonType: .numeric)
                 case "1's", "2's":
                     button = CalculatorButton(calcButtonType: .complement)
