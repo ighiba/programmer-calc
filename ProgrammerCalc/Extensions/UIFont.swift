6027e3b99ef3f0f8e0890bfa5da78c4d5b374202
ighiba <ighiba18@gmail.com>
2023-08-25 22:31:27 +0300

    Added UIFont extension

---
+++ ProgrammerCalc/Extensions/UIFont.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont.swift
+//  ProgrammerCalc
+//
+//  Created by Ivan Ghiba on 25.08.2023.
+//  Copyright © 2023 ighiba. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    static let calculatorLabelFont = UIFont(name: "HelveticaNeue-Thin", size: 70.0)
+    static let infoLabelFont = UIFont(name: "HelveticaNeue-Thin", size: 12.0)
+}
