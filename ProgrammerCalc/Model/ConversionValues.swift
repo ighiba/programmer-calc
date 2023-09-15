c9dc5cd2e55eeb3a18186cccb1dd9b5139bdd8a0
ighiba <ighiba18@gmail.com>
2023-09-16 02:03:00 +0300

    Refactored ConversionValues

--- ProgrammerCalc/Model/ConversionValues.swift
+++ ProgrammerCalc/Model/ConversionValues.swift
@@ -8,33 +8,33 @@
 
 import Foundation
 
-class ConversionValues {
-    
-    static func getForbiddenValues() -> [ConversionSystemsEnum : Set<String>] {
-        
-        let digitValues: Set<String> = ["00","0","1","2","3","4","5","6","7","8","8","9"]
-        let charValues: Set<String> = ["A","B","C","D","E","F","FF"]
-        let forbidden: [ConversionSystemsEnum : Set<String>]
-        
-        forbidden = [ .bin : digitValues.union(charValues).subtracting(["1","0","00"]),
-                      .dec : charValues,
-                      .oct: charValues.union(["8","9"]),
-                      .hex: []]
+private let digitValues: Set<String> = ["00", "0", "1", "2", "3", "4", "5", "6", "7", "8", "8", "9"]
+private let charValues: Set<String> = ["A", "B", "C", "D", "E", "F", "FF"]
 
-        return forbidden
+class ConversionValues {
+    static var allowed: [ConversionSystemsEnum : Set<String>] {
+        return [
+            .bin : digitValues.intersection(["00", "0", "1"]),
+            .dec : digitValues,
+            .oct : digitValues.subtracting(["8", "9"]),
+            .hex : digitValues.union(charValues)
+        ]
     }
     
-    static func getAllowedValues() -> [ConversionSystemsEnum : Set<String>] {
-        
-        let digitValues: Set<String> = [".","-","00","0","1","2","3","4","5","6","7","8","8","9"]
-        let charValues: Set<String> = ["A","B","C","D","E","F","FF"]
-        let allowed: [ConversionSystemsEnum : Set<String>]
-        
-        allowed = [ .bin : digitValues.intersection(["00","0","1"]),
-                      .dec : digitValues,
-                      .oct: digitValues.subtracting(["8","9"]),
-                      .hex: digitValues.union(charValues)]
-
-        return allowed
+    static var forbidden: [ConversionSystemsEnum : Set<String>] {
+        return [
+            .bin : digitValues.union(charValues).subtracting(["1", "0", "00"]),
+            .dec : charValues,
+            .oct : charValues.union(["8", "9"]),
+            .hex : []
+        ]
+    }
+    
+    static func getForbidden(for system: ConversionSystemsEnum) -> Set<String> {
+        return forbidden[system]!
+    }
+    
+    static func getAllowed(for system: ConversionSystemsEnum) -> Set<String> {
+        return allowed[system]!
     }
 }
