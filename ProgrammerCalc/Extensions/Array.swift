1b60c96e8ea4ea929afd6dbdbc974401e4ecc172
ighiba <ighiba18@gmail.com>
2023-12-05 21:06:36 +0300

    Added adjusted() and expanded() methods for Array

--- ProgrammerCalc/Extensions/Array.swift
+++ ProgrammerCalc/Extensions/Array.swift
@@ -9,11 +9,29 @@
 import Foundation
 
 extension Array {
+    func adjusted(toCount targetCount: Int, repeatingElement element: Element) -> Self {
+        guard self.count != targetCount else { return self }
+        
+        if self.count > targetCount {
+            return contracted(toCount: targetCount)
+        } else {
+            return expanded(toCount: targetCount, repeatingElement: element)
+        }
+    }
+    
     func contracted(toCount targetCount: Int) -> Self {
         guard self.count > targetCount else { return self }
 
         let range = 0..<targetCount
         
         return Self(self[range])
+    }
+
+    func expanded(toCount targetCount: Int, repeatingElement element: Element) -> Self {
+        guard self.count < targetCount else { return self }
+        
+        let needToExpandCount = targetCount - self.count
+        
+        return self + Self(repeating: element, count: needToExpandCount)
     }
 }
